b35d6af4c072a065589ddadd46cdc67f0d5c74af
ravinaveen07 <ravindharnatarajan@gmail.com>
2024-11-29 21:51:39 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CardsComponent, HeaderComponent]
+
 })
 export class AppComponent {
   title = 'Task';
---
+++ src/app/cards/cards.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrl: './cards.component.css',
+  imports:[CommonModule]
+})
+export class CardsComponent {
+
+  testimonials = [
+    {
+      image: 'assets/JacobJones.jpg',
+      name: 'Jacob Jones',
+      feedback: 'As a budding startup, we sought a financial solution that was simple, secure, and scalable...'
+    },
+    
+    {
+      image: 'assets/MarvinMcKinney.jpg',
+      name: 'Theresa Webb',
+      feedback: 'Swift Revel empowers me to manage my business finances with confidence and ease...'
+    },
+    {
+      image: 'assets/JacobJones.jpg',
+      name: 'Ronald Richards',
+      feedback: 'We needed a payment gateway that could handle our diverse customer base. Swift Revel offers a wide range of payment options, making it easy for our clients to pay'
+    },
+    {
+      image: 'assets/JacobJones.jpg',
+      name: 'Cody Fisher',
+      feedback: 'Since switching to swift revel, i finally have peace of mind regarding my business finances. with their secure platform, i trust my datas protection completely.'
+    },
+
+    {
+      image: 'assets/MarvinMcKinney.jpg',
+      name: 'Marvin McKinney',
+      feedback: 'Since switching to Swift Revel, I finally have peace of mind regarding my business finances...'
+    },
+  ];
+  
+  carouselAnimation = 'carousel 15s linear infinite';
+}
